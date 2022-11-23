aebd04ff9cb8527a996b20a18aa59683294aa9a6
didar07 <didarul111015@gmail.com>
2022-11-24 00:46:34 +0600

    benifit started

--- src/Pages/Home/Benifit/Benifit.js
+++ src/Pages/Home/Benifit/Benifit.js
@@ -5,6 +5,7 @@ const Benifit = () => {
         <div>
             <h1 className='text-3xl font-bold'>What is Assured cars?</h1>
             <p className='font-semibold'>You focus on choosing the car we take care of everything else</p>
+
         </div>
     );
 };
