f5263573a95c8fcca6f870f21e4615a6347e467e
didar07 <didarul111015@gmail.com>
2022-11-24 00:28:38 +0600

    benifit section added

--- src/Pages/Error/Error.js
+++ src/Pages/Error/Error.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 const Error = () => {
     return (
         <div className='text-center'>
-            <h2 className='text-5xl text-red-600 font-bold mt-5'>Oppssss!!! 404 Not Found</h2>
-            <img src="https://tse1.mm.bing.net/th?id=OIP.yYBFzWZ0R970KK2bJhwO9AHaEi&pid=Api&P=0" className='mx-auto w-2/3 h-1/3' alt="" />
-            <Link to='/' className='mt-5 bg-gradient-to-r from-purple-500 to-pink-500 px-7 py-3 rounded-lg text-white font-semibold'>Back to Home page</Link>
+            <h2 className='text-3xl text-red-600 font-bold'>Oppssss!!! 404 Not Found</h2>
+            <img src="https://img.freepik.com/free-vector/internet-network-warning-404-error-page-file-found-web-page-internet-error-page-issue-found-network-404-error-present-by-man-sleep-display_1150-55450.jpg?w=996&t=st=1669227439~exp=1669228039~hmac=4e2bf54344fb445e5cd14410b5c14420446a83939321078a99b806a85b2f0c14" className='mx-auto w-2/3 h-2/3' alt="" />
+            <Link to='/' className='bg-gradient-to-r from-purple-500 to-pink-500 px-7 py-3 rounded-lg text-white font-semibold shadow-lg'>Back to Home page</Link>
         </div>
     );
 };
---
+++ src/Pages/Home/Benifit/Benifit.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Benifit = () => {
+    return (
+        <div>
+            <h1 className='text-3xl font-bold'>What is Assured cars?</h1>
+            <p className='font-semibold'>You focus on choosing the car we take care of everything else</p>
+        </div>
+    );
+};
+
+export default Benifit;
