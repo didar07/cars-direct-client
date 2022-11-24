a22fa3e6e4ec1daf3e7132ba61b8fab9c1e5cf89
didar07 <didarul111015@gmail.com>
2022-11-25 00:00:52 +0600

    signup implemented

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../contexts/AuthProvider';
 
 const Navbar = () => {
 
+    const { user, logOut } = useContext(AuthContext)
+
+    const handleLogOut = () => {
+        logOut()
+            .then(() => { })
+            .catch(error => console.log(error))
+    }
+
     const menuItems = <>
         <li><Link to='/'>Home</Link></li>
-        <li><Link to='/login'>Login</Link></li>
         <li><Link to='/blogs'>Blogs</Link></li>
+        {user?.uid ?
+            <li><button onClick={handleLogOut}>Signout</button></li>
+            :
+            <li><Link to='/login'>Login</Link></li>
+        }
     </>
 
     return (
--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,5 +1,5 @@
-import React, { createContext, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth'
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext()
@@ -20,8 +20,24 @@ const AuthProvider = ({ children }) => {
         return signInWithEmailAndPassword(auth, email, password);
     }
 
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
 
-    const authInfo = { createUser, signIn, user, loading }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log('user observing');
+            setUser(currentUser);
+            setLoading(false);
+        });
+
+        return () => unsubscribe();
+    }, [])
+
+
+    const authInfo = { createUser, signIn, logOut, user, loading }
     return (
         <div>
             <AuthContext.Provider value={authInfo}>
