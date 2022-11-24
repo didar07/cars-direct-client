2459c0a181cd6ab2789acb7ef04a04d50cf91259
didar07 <didarul111015@gmail.com>
2022-11-24 17:26:30 +0600

    category added

--- src/Pages/Home/Category/Category.js
+++ src/Pages/Home/Category/Category.js
@@ -15,25 +15,20 @@ const Category = () => {
         <div>
             <h2 className='text-center text-3xl font-bold'>categories of our collection</h2>
             <div className=' mt-5 grid grid-cols-1 lg:grid-cols-3 gap-10'>
-                <Link to={`/categories/${categories[0]?._id}`}>
-                    <button className='bg-gradient-to-r from-purple-500 to-pink-500 px-7 py-3 rounded-lg text-white font-semibold'>{categories[0]?.category_name}</button>
-                </Link>
-                <Link to={`/categories/${categories[1]?._id}`}>
-                    <button className='bg-gradient-to-r from-purple-500 to-pink-500 px-7 py-3 rounded-lg text-white font-semibold'>{categories[1]?.category_name}</button>
-                </Link>
-                <Link to={`/categories/${categories[2]?._id}`}>
-                    <button className='bg-gradient-to-r from-purple-500 to-pink-500 px-7 py-3 rounded-lg text-white font-semibold'>{categories[2]?.category_name}</button>
-                </Link>
+                <button className='bg-gradient-to-r from-purple-500 to-pink-500 px-7 py-3 rounded-lg text-white font-semibold'>  <Link to={`/categories/${categories[0]?._id}`}>{categories[0]?.category_name}</Link>
+                </button>
+
+                <button className='bg-gradient-to-r from-purple-500 to-pink-500 px-7 py-3 rounded-lg text-white font-semibold'>
+                    <Link to={`/categories/${categories[1]?._id}`}>{categories[1]?.category_name}</Link>
+                </button>
+                <button className='bg-gradient-to-r from-purple-500 to-pink-500 px-7 py-3 rounded-lg text-white font-semibold'>
+                    <Link to={`/categories/${categories[2]?._id}`}>
+                        {categories[2]?.category_name}
+                    </Link>
+                </button>
             </div>
-            <div className='invisible'>
-                {
-                    <SingleCategory
-                        key={categories._id}
-                        categories={categories}
-                    ></SingleCategory>
-                }
-            </div>
-        </div>
+
+        </div >
     );
 };
 
--- src/Pages/Home/Category/SingleCategory.js
+++ src/Pages/Home/Category/SingleCategory.js
@@ -1,19 +1,41 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
-const SingleCategory = ({ categories }) => {
-    // const { first_car } = useLoaderData()
-    console.log(categories[1])
+const SingleCategory = () => {
+    const { first_car, second_car } = useLoaderData()
+    console.log(first_car)
     return (
-        <div className="card w-96 bg-base-100 shadow-xl">
-            <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-            <div className="card-body">
-                <h2 className="card-title"></h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+        <div className='lg:grid grid-cols-2 gap-7'>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <figure><img src={first_car.image} alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Car Title:{first_car.name}</h2>
+                    <p>Location: {first_car.location}</p>
+                    <p>Original Price: ${first_car.original_price}</p>
+                    <p>Resale Price: ${first_car.resale_price}</p>
+                    <p>Seller Name: {first_car.seller_name}</p>
+
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div>
                 </div>
             </div>
+
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <figure><img src={second_car.image} alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Car Title:{second_car.name}</h2>
+                    <p>Location: {second_car.location}</p>
+                    <p>Original Price: ${second_car.original_price}</p>
+                    <p>Resale Price: ${second_car.resale_price}</p>
+                    <p>Seller Name: {second_car.seller_name}</p>
+
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
